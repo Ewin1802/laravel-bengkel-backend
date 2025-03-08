9d4cd29aca00c5c612fc28de3ff3ea82ade110d9
Ewin1802 <elantapa@gmail.com>
2025-03-08 12:07:54 +0800

    gambar nota

--- resources/views/pages/product_ins/index.blade.php
+++ resources/views/pages/product_ins/index.blade.php
@@ -108,7 +108,7 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="imageModalLabel">Gambar Barang</h5>
+                    <h5 class="modal-title" id="imageModalLabel">Bukti Transaksi (Nota)</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
